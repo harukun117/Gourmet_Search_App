d95287d85a7639f5c19578eb41f82e9ab03364fe
HaruNakano <haru0117.nkn@gmail.com>
2023-11-21 11:33:28 +0900

    検索結果をリストで表示するためのviewの実装

---
+++ Gourmet_Search_App/Views/ResultStoreListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultStoreListTableViewCell.swift
+//  Gourmet_Search_App
+//
+//  Created by Nakano Haru on 2023/11/21.
+//
+
+import UIKit
+
+class ResultStoreListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Gourmet_Search_App/Views/ResultStoreListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultStoreListViewController.swift
+//  Gourmet_Search_App
+//
+//  Created by Nakano Haru on 2023/11/21.
+//
+
+import Foundation
